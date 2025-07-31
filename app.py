2b5e744a00582133a8338608c4fdbb4539c12232
revoltadinho <santosmfalfredo@gmail.com>
2025-07-31 18:05:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,17 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Carrega as variáveis do ficheiro .env
+# Carrega variáveis do .env
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
 
 # Comando /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 Olá! Eu sou o RevoltadinhoBot. Estou online e pronto para te ajudar!")
 
-# Inicializa a aplicação
-def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-
-    print("🤖 RevoltadinhoBot está online...")
-    app.run_polling()
-
+# Main
 if __name__ == '__main__':
-    main()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("🤖 RevoltadinhoBot está online...")
+    app.run_polling() 
